8d000ec1bedd5f2edd6059da601619a885f099a6
Allemand1001DK <alexped1212@gmail.com>
2024-10-23 12:12:47 +0200

    UnitTest4 error

---
+++ Disaheim/Disaheim/MerchandiseRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilityLib;
+
+namespace Disaheim
+{
+    public class MerchandiseRepository : Merchandise
+    {
+        private List<Merchandise> merchandises = new List<Merchandise>();
+
+        public void AddMerchandise(Merchandise merchandise)
+        {
+            merchandises.Add(merchandise);
+        }
+
+        public Merchandise GetMerchandise(string itemId)
+        {
+            foreach (var merchandise in merchandises)
+            {
+                if (merchandise.ItemId == itemId)
+                {
+                    return merchandise;
+                }
+            }
+            return null;
+        }
+
+        public double GetTotalValue()
+        {
+            Utility utility = new Utility();
+            double value = 0;
+            foreach (Merchandise merchandise in merchandises)
+            {
+                value = +utility.GetValueOfMerchandise(merchandise);
+            }
+            return value;
+        }
+    }
+}
--- Disaheim/Disaheim/Utility.cs
+++ Disaheim/Disaheim/Utility.cs
@@ -10,23 +10,24 @@ namespace UtilityLib
 {
     public class Utility
     {
-        public double GetValueOfBook(Book book)
-        {
-            return book.Price;
-        }
+        //public double GetValueOfBook(Book book)
+        //{
+        //    return book.Price;
+        //}
 
-        public double GetValueOfAmulet(Amulet amulet)
-        {
-            //Output:	Amulettens værdi returneres.
-            // Værdien fastsættes ud fra amulettens kvalitet: 
-            //low-> 12.5, medium-> 20.0 og high -> 27.5
-            return amulet.Quality switch
-            {
-                Level.low => 12.5,
-                Level.medium => 20.0,
-                Level.high => 27.5,
-            };
-        }
+        //public double GetValueOfAmulet(Amulet amulet)
+        //{
+        //    //Output:	Amulettens værdi returneres.
+        //    // Værdien fastsættes ud fra amulettens kvalitet: 
+        //    //low-> 12.5, medium-> 20.0 og high -> 27.5
+        //    return amulet.Quality switch
+        //    {
+        //        Level.low => 12.5,
+        //        Level.medium => 20.0,
+        //        Level.high => 27.5,
+        //    };
+        //}
+        private List<Merchandise> merchandises = new List<Merchandise>();
 
         public double GetValueOfCourse(Course course)
         {
@@ -36,6 +37,24 @@ namespace UtilityLib
                 hours++;
             }
             return hours * 875;
+        }
+
+        public double GetValueOfMerchandise(Merchandise merchandise)
+        {
+            if (merchandise is Amulet amulet)
+            {
+                return amulet.Quality switch
+                {
+                    Level.low => 12.5,
+                    Level.medium => 20.0,
+                    Level.high => 27.5,
+                };
+            }else if (merchandise is Book book)
+            {
+                return book.Price;
+            }
+
+            return 0;
         }
     }
 }
